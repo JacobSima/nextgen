a34cdba811d3945a53b73d4a02acb8a2357000e4
Jacob K Sima <simajacob2011@gmail.com>
2025-02-24 22:57:12 +0200

    TEST-16, Complete all test cases

--- PaySpace.Calculator.Tests/FlatRateCalculatorTests.cs
+++ PaySpace.Calculator.Tests/FlatRateCalculatorTests.cs
@@ -1,29 +1,35 @@
-﻿using Moq;
-
-using NUnit.Framework;
-
-namespace PaySpace.Calculator.Tests
+﻿namespace PaySpace.Calculator.Tests
 {
-    [TestFixture]
-    internal sealed class FlatRateCalculatorTests
+  using NUnit.Framework;
+  using PaySpace.Calculator.Services.Abstractions;
+  using PaySpace.Calculator.Services.Calculators;
+
+  [TestFixture]
+  internal sealed class FlatRateCalculatorTests
+  {
+    private IFlatRateCalculator flatRateCalculator;
+    private static readonly List<(decimal from, decimal to, decimal rate)> settings =
+    [
+      (0m, decimal.MaxValue, 17.5m)
+    ];
+
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [TestCase(-1, 0)]
-        [TestCase(999999, 174999.825)]
-        [TestCase(1000, 175)]
-        [TestCase(5, 0.875)]
-        public async Task Calculate_Should_Return_Expected_Tax(decimal income, decimal expectedTax)
-        {
-            // Arrange
-
-            // Act
-
-
-            // Assert
-        }
+      this.flatRateCalculator = new FlatRateCalculator(settings);
     }
+
+    [TestCase(-1, 0)]
+    [TestCase(999999, 174999.825)]
+    [TestCase(1000, 175)]
+    [TestCase(5, 0.875)]
+    public async Task Calculate_Should_Return_Expected_Tax(decimal income, decimal expectedTax)
+    {
+      // Act
+      var tax = this.flatRateCalculator.CalculateTax(income);
+
+      // Assert
+      Assert.That(tax, Is.EqualTo(expectedTax));
+    }
+  }
 }
--- PaySpace.Calculator.Tests/ProgressiveCalculatorTests.cs
+++ PaySpace.Calculator.Tests/ProgressiveCalculatorTests.cs
@@ -1,13 +1,27 @@
 ﻿namespace PaySpace.Calculator.Tests
 {
   using NUnit.Framework;
+  using PaySpace.Calculator.Services.Abstractions;
+  using PaySpace.Calculator.Services.Calculators;
 
   [TestFixture]
   internal sealed class ProgressiveCalculatorTests
   {
+    private IProgressiveCalculator progressiveCalculator;
+    private static readonly List<(decimal from, decimal to, decimal rate)> settings =
+    [
+      (0m, 8350m, 10m),
+      (8351m, 33950m, 15m),
+      (33951m, 82250m, 25m),
+      (82251m, 171550m, 28m),
+      (171551m, 372950m, 33m),
+      (372951m, decimal.MaxValue, 35m)
+    ];
+
     [SetUp]
     public void Setup()
     {
+      this.progressiveCalculator = new ProgressiveCalculator(settings);
     }
 
     [TestCase(-1, 0)]
@@ -20,11 +34,11 @@
     [TestCase(999999, 327683.15)]
     public async Task Calculate_Should_Return_Expected_Tax(decimal income, decimal expectedTax)
     {
-      // Arrange
-
       // Act
+      var tax = this.progressiveCalculator.CalculateTax(income);
 
       // Assert
+      Assert.That(tax, Is.EqualTo(expectedTax));
     }
   }
 }
